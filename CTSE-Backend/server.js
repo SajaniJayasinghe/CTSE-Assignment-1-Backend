0669c320024efb6e95e981f40c7264a68b5e0199
Sajani Jayasinghe <jayasinghesajani98@gmail.com>
2023-03-19 15:27:00 +0530

    sample

--- CTSE-Backend/server.js
+++ CTSE-Backend/server.js
@@ -4,6 +4,7 @@ const { connection } = require("./utils/connection");
 const dotenv = require("dotenv");
 
 //@import router
+const SampleRouter = require("./routes/sample.route");
 
 const app = express();
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 //use routes
+app.use("/api/sample/", SampleRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port number: ${PORT}`);
