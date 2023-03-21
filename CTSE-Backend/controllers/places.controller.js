0a3269be7162fd361165fe3f9a25806bbdb3a48d
Aroshini88 <arodeez88@gmail.com>
2023-03-21 23:37:05 +0530

    bug fix

--- CTSE-Backend/controllers/places.controller.js
+++ CTSE-Backend/controllers/places.controller.js
@@ -1,83 +1,92 @@
 const Places = require("../models/places.models");
+// const firebaseService = require("../firebase/firebase.service");
+// const firebaseUtils = require("../firebase/firebse.util");
 
 const NewPlace = async (req, res) => {
-  let newPlace = new Places(req.body);
-  Places.create(newPlace).then((place) => {
-    res
-      .status(200)
-      .json({
-        success: "New Added add Successfully !!",
-        place: place,
-      })
-      .catch((err) => {
-        res.status(400).json({
-          error: err.message,
-        });
-      });
+  // const file = req.file;
+
+  // const imageName = `Image_${Date.now()}`;
+  // const url = firebaseUtils.generateFirebaseStorageURL(imageName);
+
+  // await firebaseService.uploadToFirebase(file, imageName);
+
+  const { type, name, description, city, facilities } = req.body;
+  const createdPlace = new Places({
+    type,
+    name,
+    description,
+    city,
+    facilities
+    // picture: url
   });
+  try {
+    await createdPlace.save();
+    res.status(200).json({
+      success: true,
+      message: "Place Added Successfully !!",
+      place: createdPlace
+    });
+  } catch (err) {
+    res.status(400).json({
+      error: err
+    });
+  }
 };
 
 const GetPlace = async (req, res) => {
-  Places.find().exec((err, places) => {
-    if (err) {
-      return res.status(400).json({
-        error: err,
-      });
-    }
-    return res.status(200).json({
+  try {
+    let places = await Places.find();
+    res.status(200).json({
       success: true,
-      existingplaces: places,
+      existingplaces: places
     });
-  });
+  } catch (err) {
+    res.status(400).json({
+      error: err
+    });
+  }
 };
 
 const GetOnePlace = async (req, res) => {
-  let placeID = req.params.placeID;
-  Places.findById(placeID, (err, place) => {
-    if (err) {
-      return res.status(400).json({
-        error: err,
-      });
-    }
-    return res.status(200).json({
+  try {
+    let place = await Places.findById(req.params.placeID);
+    res.status(200).json({
       success: true,
-      existingplaces: place,
+      existingplace: place
     });
-  });
+  } catch (err) {
+    res.status(400).json({
+      error: err
+    });
+  }
 };
 
-const UpdatePlace = (req, res) => {
-  Places.findByIdAndUpdate(
-    req.params.placeID,
-    {
-      $set: req.body,
-    },
-    (err) => {
-      if (err) {
-        return res.status(400).json({
-          error: err,
-        });
-      }
-      return res.status(200).json({
-        success: "Updated Successfully",
-      });
-    }
-  );
+const UpdatePlace = async (req, res) => {
+  try {
+    const placeID = req.params.placeID;
+    const updatePlace = await Places.findByIdAndUpdate(placeID, {
+      $set: req.body
+    });
+    res.status(200).send({ success: true, updatePlace: updatePlace });
+  } catch (error) {
+    res.status(500).send({ status: "Error with id", error: error.message });
+  }
 };
 
 const DeletePlace = (req, res) => {
-  Places.findByIdAndRemove(req.params.placeID).exec((err, deleteplaces) => {
-    if (err)
-      return res.status(400).json({
-        message: "Deletion Unsuccessfull",
-        err,
+  Places.findByIdAndDelete(req.params.placeID)
+    .then((place) => {
+      res.status(200).json({
+        success: true,
+        message: "Place Deleted Successfully !!",
+        place: place
       });
-
-    return res.json({
-      message: "Deletion Successfull",
-      deleteplaces,
+    })
+    .catch((err) => {
+      res.status(400).json({
+        error: err
+      });
     });
-  });
 };
 
 module.exports = {
@@ -85,5 +94,5 @@ module.exports = {
   GetPlace,
   GetOnePlace,
   UpdatePlace,
-  DeletePlace,
+  DeletePlace
 };
