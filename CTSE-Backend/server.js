acaf3c1c3474ab4a6f2e698c734758ac4572da8a
Imaya Rajapaksha <86527015+IT20103118@users.noreply.github.com>
2023-03-22 12:31:08 +0530

    hotel backend

--- CTSE-Backend/server.js
+++ CTSE-Backend/server.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 const SampleRouter = require("./routes/sample.route");
 const UserRouter = require("./routes/user");
 const PlaceRouter = require("./routes/places");
+const HotelRouter = require("./routes/hotels");
 
 const app = express();
 dotenv.config();
@@ -19,6 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/sample/", SampleRouter);
 app.use("/api/user", UserRouter);
 app.use("/api/places", PlaceRouter);
+app.use("/api/hotels", HotelRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port number: ${PORT}`);
